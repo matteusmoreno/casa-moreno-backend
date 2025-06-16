45fe7f113d71c5bd75b0d99a92ecdd5ffa4c1d77
Matteus Moreno <matteus_moreno@live.com>
2025-06-16 10:08:22 -0300

    initial commit

---
+++ src/main/java/br/com/casa_moreno/casa_moreno_backend/CasaMorenoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.casa_moreno.casa_moreno_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaMorenoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaMorenoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/casa_moreno/casa_moreno_backend/CasaMorenoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casa_moreno.casa_moreno_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaMorenoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
