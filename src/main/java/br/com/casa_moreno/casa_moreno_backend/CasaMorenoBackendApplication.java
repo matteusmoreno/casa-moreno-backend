ecbc8badad851ae2afc07256a8358f94f532c6f8
Matteus Moreno <matteus_moreno@live.com>
2025-06-17 13:50:36 -0300

    add @EnableSpringDataWebSupport

--- src/main/java/br/com/casa_moreno/casa_moreno_backend/CasaMorenoBackendApplication.java
+++ src/main/java/br/com/casa_moreno/casa_moreno_backend/CasaMorenoBackendApplication.java
@@ -3,9 +3,11 @@ package br.com.casa_moreno.casa_moreno_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CasaMorenoBackendApplication {
 
 	public static void main(String[] args) {
