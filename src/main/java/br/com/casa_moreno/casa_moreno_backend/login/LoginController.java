eb6c463895995c0d99e5ffe0980fd00a74fb191f
Matteus Moreno <matteus_moreno@live.com>
2025-06-20 22:09:07 -0300

    test workflow

--- src/main/java/br/com/casa_moreno/casa_moreno_backend/login/LoginController.java
+++ src/main/java/br/com/casa_moreno/casa_moreno_backend/login/LoginController.java
@@ -2,6 +2,7 @@ package br.com.casa_moreno.casa_moreno_backend.login;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ public class LoginController {
         LoginResponse response = loginService.generateToken(loginRequest);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test endpoint is working!");
     }
 }
